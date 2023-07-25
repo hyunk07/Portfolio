07e6c70fcd48279941577d4dcf4f300d263dd53a
hyunk07 <gusrud3226@naver.com>
2023-07-26 01:13:10 +0900

    프로젝트3

--- Project3/Project3.js
+++ Project3/Project3.js
@@ -1,101 +1,96 @@
 // 화살버튼클릭
 //왼쪽 버튼, 이전버튼
 $(".left_arrow").on("click", () => {
-   $(".slidItem").last().prependTo(".slideWrapper");
- });
- 
- //오른쪽 버튼, 다음버튼
- $(".right_arrow").on("click", () => {
-     $(".slidItem").first().appendTo(".slideWrapper");
-   });
+  $(".slidItem").last().prependTo(".slideWrapper");
+});
 
+//오른쪽 버튼, 다음버튼
+$(".right_arrow").on("click", () => {
+  $(".slidItem").first().appendTo(".slideWrapper");
+});
 
 //  원형버튼클릭
 let currentIdx = 0;
 
-$('.slideBtn li').on('click', e => {
-   const target = $(e.target);
-   const idx = target.index();
+$(".slideBtn li").on("click", (e) => {
+  const target = $(e.target);
+  const idx = target.index();
 
-   $('.slideWrapper').stop().animate({
-      marginLeft: -100 * idx + '%'
-   }, 700);
+  $(".slideWrapper")
+    .stop()
+    .animate(
+      {
+        marginLeft: -100 * idx + "%",
+      },
+      700
+    );
 
-   $('.slideBtn li').removeClass('active');
-   target.addClass('active');
+  $(".slideBtn li").removeClass("active");
+  target.addClass("active");
 
-   currentIdx = idx; //현재 인덱스에 내가 클릭한 인덱스 번호 대입
-})
+  currentIdx = idx; //현재 인덱스에 내가 클릭한 인덱스 번호 대입
+});
 
 const autoSlide = () => {
-   currentIdx = (currentIdx + 1) % 3; //0,1,2,3
+  currentIdx = (currentIdx + 1) % 3; //0,1,2,3
 
-   $('.slideWrapper').stop().animate({
-      marginLeft: -100 * currentIdx + '%'
-   }, 700);
+  $(".slideWrapper")
+    .stop()
+    .animate(
+      {
+        marginLeft: -100 * currentIdx + "%",
+      },
+      700
+    );
 
-   // $('.slideBtn li').eq(currentIdx).addClass('.active');
+  // $('.slideBtn li').eq(currentIdx).addClass('.active');
 
-   $('.slideBtn li').eq(currentIdx).addClass('active').siblings().removeClass('active')
-}
-
-
+  $(".slideBtn li")
+    .eq(currentIdx)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+};
 
 let mainSlide = setInterval(autoSlide, 4000);
 
-$('.info_right').hover(function () {
-   clearInterval(mainSlide);
-}, function () {
-   mainSlide = setInterval(autoSlide, 3000);
+$(".info_right").hover(
+  function () {
+    clearInterval(mainSlide);
+  },
+  function () {
+    mainSlide = setInterval(autoSlide, 3000);
+  }
+);
+
+/* ----------exhibition---------- */
+const mySwiper1 = new Swiper(".mySwiper1", {
+  loop: true, // 무한 반복
+
+  pagination: {
+    // 하단 페이지네이션
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    // 좌 우 화살표
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
-
-// -----------------------
-
-// 화살버튼클릭
-//왼쪽 버튼, 이전버튼
-$("docent_left_arrow").on("click", () => {
-   $(".slidItem").last().prependTo(".gallery");
- });
- 
- //오른쪽 버튼, 다음버튼
- $(".docent_right_arrow") .last().on("click", () => {
-     $(".slidItem").first().appendTo(".gallery");
-   });
-
-/* ------------------ */
-let i = 0;
-function nextSlide() {
-   i++;
-
-   //i의 숫자가 슬라이드 이미지가 가진 숫자보다 커지면
-   if (i >= $('.slideWrapper .slidItem').length) {
-      i = 0; //다시 인덱스 번호 0으로 돌아와 처음 이미지가 보이게
-   }
-
-}
-$('#nextBtn').on('click', nextSlide);
-
-
-
-/* prevSlide */
-function prevSlide() {
-   i--;
-   if (i < 0) {
-      i = $('.slideWrapper .slidItem').length - 1;
-   }
-
-}
-$('#prevBtn').on('click', prevSlide);
-
-
-
-
-
-
-setInterval(nextSlide, 3000);
-
-
-
-
-/* ------------------ */
+/* ----------docent---------- */
+const mySwiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  pagination: {
+    // 하단 페이지네이션
+    el: ".mySwiper2 .swiper-pagination",
+    clickable: true,
+  },
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
